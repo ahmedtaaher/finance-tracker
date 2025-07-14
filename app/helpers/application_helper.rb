cf457d990bfdd6f4aa66420064e90f56fd6d3494
Ahmed Taher <ahmedtaherferweez@gmail.com>
2025-07-14 21:18:30 +0300

    Add views using devise bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def bootstrap_devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    html = <<-HTML
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <ul class="mb-0">#{messages}</ul>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
