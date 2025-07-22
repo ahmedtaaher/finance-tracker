de5523e2aeeee14770581b8a76fd8748a8404adb
Ahmed Taher <ahmedtaherferweez@gmail.com>
2025-07-22 16:37:44 +0300

    Add stocks listing view

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  validates :ticker, :name, presence: true
   def self.new_lookup(symbol)
     quote = StockService.get_quote(symbol)
     return nil if quote.blank? || quote["05. price"].blank?
